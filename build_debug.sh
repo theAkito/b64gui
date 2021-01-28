dfcd2641beb150e86602f395eafe4ad0fb842825
Akito <the@akito.ooo>
2021-01-28 20:54:12 +0100

    Encode Files and save output Files to Directory

---
+++ build_debug.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+nimble dbuild && \
+mv b64gui.exe /mnt/c/Users/Akito/Desktop/b64gui.exe
--- src/b64gui.nim
+++ src/b64gui.nim
@@ -7,13 +7,62 @@ import
 proc encodeFilesToText(filePaths: seq[string]): seq[string] =
   result = filePaths.mapIt(it.lastPathPart & " " & it.readFile.encode)
 
-proc encodeFilesToFiles(filePaths: seq[string]) =
-  discard filePaths.map(
-    proc (x: string): string =
+proc encodeFilesToFiles(sourceFilePaths: seq[string], targetPath: string) =
+  sourceFilePaths.apply(
+    proc (x: string) =
       let s = x.readFile.encode
-      (x.parentDir / "b64_" & x.lastPathPart).writeFile(s)
+      (targetPath / "b64_" & x.lastPathPart).writeFile(s)
   )
 
 when isMainModule:
-  echo "Greetings!"
-  5000.sleep
+  app.init
+  var
+    fileNames: seq[string]
+    filePaths: seq[string]
+    window = newWindow("Base64 GUI")
+    container = newLayoutContainer(Layout_Vertical)
+    button_selectFiles = newButton("Select Files...")
+    button_selectDirectory = newButton("Select Directory...")
+    textArea = newTextArea()
+  window.width = 600.scaleToDpi
+  window.height = 400.scaleToDpi
+  window.add(container)
+  container.add(button_selectFiles)
+  container.add(button_selectDirectory)
+  container.add(textArea)
+  button_selectFiles.onClick = proc(event: ClickEvent) =
+    var dialog = newOpenFileDialog()
+    dialog.title = "Select files that are to be encoded."
+    dialog.multiple = true
+    # dialog.startDirectory = ""
+    dialog.run()
+    if dialog.files == @[]:
+      textArea.addLine("No files selected.")
+    else:
+      dialog.files.apply(
+        proc (x: string) =
+          let fileName = x.lastPathPart
+          filePaths.add(x)
+          fileNames.add(fileName)
+          textArea.addLine(fileName)
+      )
+      let saveFilesDialog = SelectDirectoryDialog(
+        title: "Save Files to..."
+      )
+      saveFilesDialog.run
+      if saveFilesDialog.selectedDirectory == "":
+        textArea.addLine("No directory selected.")
+      else:
+        let targetDir = saveFilesDialog.selectedDirectory
+        filePaths.encodeFilesToFiles(targetDir)
+  button_selectDirectory.onClick = proc(event: ClickEvent) =
+    var dialog = SelectDirectoryDialog()
+    dialog.title = "Select directory containing files that are to be encoded."
+    # dialog.startDirectory = ""
+    dialog.run()
+    if dialog.selectedDirectory == "":
+      textArea.addLine("No directory selected.")
+    else:
+      textArea.addLine(dialog.selectedDirectory)
+  window.show()
+  app.run()
