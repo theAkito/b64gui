a0f9a9fe80519a8d18f778cb8808debbf557bc3c
Akito <the@akito.ooo>
2021-01-28 16:17:45 +0100

    Add encode procs

--- src/b64gui.nim
+++ src/b64gui.nim
@@ -1,4 +1,18 @@
+import
+  base64,
+  nigui,
+  sequtils,
+  os
 
+proc encodeFilesToText(filePaths: seq[string]): seq[string] =
+  result = filePaths.mapIt(it.lastPathPart & " " & it.readFile.encode)
+
+proc encodeFilesToFiles(filePaths: seq[string]) =
+  discard filePaths.map(
+    proc (x: string): string =
+      let s = x.readFile.encode
+      (x.parentDir / "b64_" & x.lastPathPart).writeFile(s)
+  )
 
 when isMainModule:
   echo "Greetings!"
