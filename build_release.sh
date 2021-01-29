bf5f46a6e1659bc2109e987583f7fb42fc69f7f6
Akito <the@akito.ooo>
2021-01-29 14:40:30 +0100

    Add Linux build to build script

--- build_release.sh
+++ build_release.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Windows
 nimble fbuild && \
 strip b64gui.exe && \
 mv b64gui.exe /mnt/c/Users/Akito/Desktop/b64gui.exe
+
+# Linux amd64
+nim c \
+  --define:danger \
+  --opt:speed \
+  --app:gui \
+  --out:b64gui \
+  src/b64gui && \
+strip b64gui && \
+mv b64gui /mnt/c/Users/Akito/Desktop/b64gui_linux_x64
