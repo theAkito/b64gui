0a1ef236456be8f66ba162da005b8536a4e150af
Akito <the@akito.ooo>
2021-01-29 13:24:21 +0100

    Make Code much more readable & Clean Up

--- src/b64gui.nim
+++ src/b64gui.nim
@@ -8,13 +8,52 @@ proc longLineBreak(): string = "\p\p\p"
 proc shortLineBreak(): string = "\p\p"
 
 proc encodeFilesToText(filePaths: seq[string]): seq[string] =
-  result = filePaths.mapIt(it.lastPathPart & shortLineBreak() & it.readFile.encode)
+  filePaths.mapIt(it.lastPathPart & shortLineBreak() & it.readFile.encode)
 
 proc encodeFilesToFiles(sourceFilePaths: seq[string], targetPath: string) =
   sourceFilePaths.apply(
     proc (x: string) =
-      let s = x.readFile.encode
-      (targetPath / "b64_" & x.lastPathPart).writeFile(s)
+      (targetPath / "b64_" & x.lastPathPart).writeFile(x.readFile.encode)
+  )
+
+template resetFileCollections() =
+  filePaths = @[]
+  fileNames = @[]
+
+template saveOutputFiles() =
+  let saveFilesDialog = SelectDirectoryDialog(
+    title: "Save Files to..."
+  )
+  saveFilesDialog.run
+  if saveFilesDialog.selectedDirectory == "":
+    textArea.addLine("No directory selected.")
+  else:
+    let targetDir = saveFilesDialog.selectedDirectory
+    filePaths.encodeFilesToFiles(targetDir)
+    resetFileCollections
+
+template printFilesToText() =
+  filePaths.encodeFilesToText.apply(
+    proc (x: string) =
+      textArea.addLine(x)
+      textArea.addLine(longLineBreak())
+  )
+  resetFileCollections
+
+template getFilesFromDir() =
+  for kind, file in dialog.selectedDirectory.walkDir:
+    if kind != pcFile: continue
+    filePaths.add(file)
+    fileNames.add(file.lastPathPart)
+
+template getSelectedFiles(printFilesToTextArea: bool): untyped =
+  dialog.files.apply(
+    proc (x: string) =
+      let fileName = x.lastPathPart
+      filePaths.add(x)
+      fileNames.add(fileName)
+      if printFilesToTextArea:
+        textArea.addLine(fileName)
   )
 
 when isMainModule:
@@ -30,109 +69,59 @@ when isMainModule:
     button_selectDirectoryToFiles = newButton("Select Directory and Save Files as Files")
     button_clearTextArea = newButton("Clear Text Area")
     textArea = newTextArea()
+  let
+    controllers: seq[ControlImpl] = @[
+      button_selectFilesToText,
+      button_selectFilesToFiles,
+      button_selectDirectoryToText,
+      button_selectDirectoryToFiles,
+      button_clearTextArea,
+      textArea
+    ]
   textArea.editable = false
   textArea.wrap = false
   window.width = 600.scaleToDpi
   window.height = 400.scaleToDpi
   window.add(container)
-  container.add(button_selectFilesToText)
-  container.add(button_selectFilesToFiles)
-  container.add(button_selectDirectoryToText)
-  container.add(button_selectDirectoryToFiles)
-  container.add(button_clearTextArea)
-  container.add(textArea)
-  button_clearTextArea.onClick = proc(event: ClickEvent) =
-    textArea.text=""
+  controllers.apply(proc(x: ControlImpl) = container.add(x))
+  button_clearTextArea.onClick = proc(event: ClickEvent) = textArea.text = ""
   button_selectFilesToFiles.onClick = proc(event: ClickEvent) =
     var dialog = newOpenFileDialog()
     dialog.title = "Select files that are to be encoded."
     dialog.multiple = true
-    # dialog.startDirectory = ""
     dialog.run()
     if dialog.files == @[]:
       textArea.addLine("No files selected.")
     else:
-      dialog.files.apply(
-        proc (x: string) =
-          let fileName = x.lastPathPart
-          filePaths.add(x)
-          fileNames.add(fileName)
-          textArea.addLine(fileName)
-      )
-      let saveFilesDialog = SelectDirectoryDialog(
-        title: "Save Files to..."
-      )
-      saveFilesDialog.run
-      if saveFilesDialog.selectedDirectory == "":
-        textArea.addLine("No directory selected.")
-      else:
-        let targetDir = saveFilesDialog.selectedDirectory
-        filePaths.encodeFilesToFiles(targetDir)
-        filePaths = @[]
-        fileNames = @[]
+      getSelectedFiles(true)
+      saveOutputFiles
   button_selectFilesToText.onClick = proc(event: ClickEvent) =
     var dialog = newOpenFileDialog()
     dialog.title = "Select files that are to be encoded."
     dialog.multiple = true
-    # dialog.startDirectory = ""
     dialog.run()
     if dialog.files == @[]:
       textArea.addLine("No files selected.")
     else:
-      dialog.files.apply(
-        proc (x: string) =
-          let fileName = x.lastPathPart
-          filePaths.add(x)
-          fileNames.add(fileName)
-      )
-      filePaths.encodeFilesToText.apply(
-        proc (x: string) =
-          textArea.addLine(x)
-          textArea.addLine(longLineBreak())
-      )
-      filePaths = @[]
-      fileNames = @[]
+      getSelectedFiles(false)
+      printFilesToText
   button_selectDirectoryToText.onClick = proc(event: ClickEvent) =
     var dialog = SelectDirectoryDialog()
     dialog.title = "Select directory containing files that are to be encoded."
-    # dialog.startDirectory = ""
     dialog.run()
     if dialog.selectedDirectory == "":
       textArea.addLine("No directory selected.")
     else:
-      for kind, file in dialog.selectedDirectory.walkDir:
-        if kind != pcFile: continue
-        filePaths.add(file)
-        fileNames.add(file.lastPathPart)
-      filePaths.encodeFilesToText.apply(
-        proc (x: string) =
-          textArea.addLine(x)
-          textArea.addLine(longLineBreak())
-      )
-      filePaths = @[]
-      fileNames = @[]
+      getFilesFromDir
+      printFilesToText
   button_selectDirectoryToFiles.onClick = proc(event: ClickEvent) =
     var dialog = SelectDirectoryDialog()
     dialog.title = "Select directory containing files that are to be encoded."
-    # dialog.startDirectory = ""
     dialog.run()
     if dialog.selectedDirectory == "":
       textArea.addLine("No directory selected.")
     else:
-      for kind, file in dialog.selectedDirectory.walkDir:
-        if kind != pcFile: continue
-        filePaths.add(file)
-        fileNames.add(file.lastPathPart)
-      let saveFilesDialog = SelectDirectoryDialog(
-        title: "Save Files to..."
-      )
-      saveFilesDialog.run
-      if saveFilesDialog.selectedDirectory == "":
-        textArea.addLine("No directory selected.")
-      else:
-        let targetDir = saveFilesDialog.selectedDirectory
-        filePaths.encodeFilesToFiles(targetDir)
-        filePaths = @[]
-        fileNames = @[]
+      getFilesFromDir
+      saveOutputFiles
   window.show()
   app.run()
